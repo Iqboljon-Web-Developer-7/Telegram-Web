d094158eb52c106e79a796c343ff7e0a121f573a
Iqboljon <iqboljonummataliyev30@gmail.com>
2024-11-07 19:28:31 +0500

    starter files for the project

---
+++ app/layout.tsx
@@ -0,0 +1,92 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const roboto = localFont({
+  src: [
+    {
+      path: "./fonts/Roboto-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Roboto-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Roboto-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Roboto-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Roboto-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Roboto-Black.ttf",
+      weight: "900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Roboto-BlackItalic.ttf",
+      weight: "900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-roboto",
+});
+
+export const metadata: Metadata = {
+  title: "Telegram Web",
+  description:
+    "Telegram Web Application. Message with your friends, family and colleagues.",
+  icons: {
+    icon: "./favicon.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} font-roboto antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
